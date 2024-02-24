08c8f863de260315cfe21acafbc38bec7c347d55
danubiobwm <danubio.bwm@gmail.com>
2024-02-24 02:17:19 -0300

    test containers golang

--- post/post.go
+++ post/post.go
@@ -8,3 +8,15 @@ type Post struct {
 	Author  string
 	DB      *sql.DB
 }
+
+func (p *Post) Create() error {
+	_, err := p.DB.Exec("INSERT INTO posts (content, author) VALUES (?, ?)", p.Content, p.Author)
+	return err
+
+}
+
+func (p *Post) GetPost(id int) (Post, error) {
+	row := p.DB.QueryRow("SELECT id, content, author FROM posts WHERE id = ?", id)
+	err := row.Scan(&p.ID, &p.Content, &p.Author)
+	return *p, err
+}
